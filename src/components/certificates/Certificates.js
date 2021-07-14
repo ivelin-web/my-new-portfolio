a29f8a98ae0e8dd2c8bd64b28922e37ed684a1c8
ivelin-web <72993041+ivelin-web@users.noreply.github.com>
2021-07-14 17:27:37 +0300

    Added two new certificates

--- src/components/certificates/Certificates.js
+++ src/components/certificates/Certificates.js
@@ -3,6 +3,8 @@ import problemSolvingCertificate from "../../images/certificates/problem-solving
 import cSharpCertificate from "../../images/certificates/c-sharp.png";
 import jsCertificate from "../../images/certificates/js.png";
 import cssCertificate from "../../images/certificates/css.png";
+import reactCertificate from "../../images/certificates/react.png";
+import angularCertificate from "../../images/certificates/angular.png";
 import Certificate from "./Certificate";
 
 export default function Certificates() {
@@ -10,10 +12,12 @@ export default function Certificates() {
         <section id="certificates" className="bg-white">
             <div className="my-container py-10">
                 <h2 className="section-heading mb-20">Certificates</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-x-6 lg:gap-20 lg:gap-x-20">
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-20 md:gap-y-12 md:gap-x-6 lg:gap-x-8">
                     <Certificate img={problemSolvingCertificate} title="Problem Solving" />
                     <Certificate img={cSharpCertificate} title="C#" />
                     <Certificate img={jsCertificate} title="JavaScript" />
+                    <Certificate img={reactCertificate} title="React" />
+                    <Certificate img={angularCertificate} title="Angular" />
                     <Certificate img={cssCertificate} title="CSS" />
                 </div>
             </div>
